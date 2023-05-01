108c194c4ec452ab3f5bee806232b08a1103bb65
norbadboy <norbadboy@gmail.com>
2023-05-01 16:41:31 +0200

    updated header for non-register

---
+++ project-exam-2/src/api/constant.mjs
@@ -0,0 +1,3 @@
+export const API_URL = "https://api.noroff.dev/api/v1";
+export const API_BASE = "/holidaze";
+export const API_PATH = `${API_URL}${API_BASE}`;
--- project-exam-2/src/components/header.jsx
+++ project-exam-2/src/components/header.jsx
@@ -1,21 +1,35 @@
 import { Link } from "react-router-dom";
-import { Navbar, Container } from "react-bootstrap";
 import logo from "../pictures/logo.png";
 import "../styles/header.css";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 function NavbarComponent() {
   return (
-    <Container className="d-flex justify-content-space-between">
-      <Navbar className="navbarContainer" fixed="top" bg="light" expand="lg">
+    <Navbar collapseOnSelect bg="light" expand="xxl" className="navbarContainer" fixed="top">
+      <Container className="d-flex justify-content-space-between">
         <div className="d-flex align-items-center">
-          <Navbar.Brand href="/">
-            {" "}
-            <img src={logo} alt="logo" className="logo" />{" "}
+          <Navbar.Brand className="logoContainer">
+            <Link to="/">
+              <img src={logo} alt="logo" className="logoImage" />
+            </Link>
           </Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      </Navbar>
-    </Container>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="me-auto">
+            <Nav.Link href="/" className="d-flex justify-content-end">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/register" className="d-flex justify-content-end">
+              <strong>Sign Up</strong>
+            </Nav.Link>
+            <Nav.Link href="/login" className="d-flex justify-content-end">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
