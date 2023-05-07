3e2ea97b4fabe667ed1332c41a112065d83f4006
norbadboy <norbadboy@gmail.com>
2023-05-07 14:13:47 +0200

    added load more button to homepage

--- project-exam-2/src/components/header.jsx
+++ project-exam-2/src/components/header.jsx
@@ -3,7 +3,7 @@ import logo from "../pictures/logo.png";
 import styles from "../styles/header.module.css";
 import { Nav, Navbar, Container } from "react-bootstrap";
 
-function NavbarComponent() {
+function LoggedOutHeader() {
   return (
     <Navbar
       collapseOnSelect
@@ -32,9 +32,6 @@ function NavbarComponent() {
             <Nav.Link href="/" className="d-flex justify-content-end">
               Home
             </Nav.Link>
-            <Nav.Link href="/admin" className="d-flex justify-content-end">
-              Holidaze your home
-            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -42,11 +39,44 @@ function NavbarComponent() {
   );
 }
 
+// function LoggedInHeader() {
+//   return (
+//     <Navbar
+//       collapseOnSelect
+//       bg="light"
+//       expand="xxxl"
+//       className={styles.navbarContainer}
+//       fixed="top"
+//     >
+//       <Container className="d-flex justify-content-space-between">
+//         <div className="d-flex align-items-center">
+//           <Navbar.Brand className="logoContainer">
+//             <Link to="/">
+//               <img src={logo} alt="logo" className="logoImage" />
+//             </Link>
+//           </Navbar.Brand>
+//         </div>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.navbarToggle} />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className={styles.navItemsContainer}>
+//             <Nav.Link href="/admin" className="d-flex justify-content-end">
+//               Holidaze your home
+//             </Nav.Link>
+//             <Nav.Link href="/" className="d-flex justify-content-end">
+//               Home
+//             </Nav.Link>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
 function Header() {
   return (
-    <header>
-      <NavbarComponent />
-    </header>
+    <div>
+      <LoggedOutHeader />
+    </div>
   );
 }
 
--- project-exam-2/src/routes/HomePage/index.jsx
+++ project-exam-2/src/routes/HomePage/index.jsx
@@ -1,12 +1,20 @@
-import { Row, Col, Card } from "react-bootstrap";
-import { API_PATH } from "../../api/constant.mjs";
+import { Row, Col, Card, Button } from "react-bootstrap";
 import ProductCard from "../../components/productCard.jsx";
+import { API_PATH } from "../../api/constant.mjs";
 import useAPI from "../../api/apiHook.jsx";
+import { useState } from "react";
+import { StyledButton } from "../../styles/styledComponents/styledButton.jsx";
 
 const url = API_PATH + "/venues";
 
 function HomePage() {
-  const { data, loading, error } = useAPI(url);
+  const [productLimit, setProductLimit] = useState(24);
+  const limit = 96;
+  const { data, loading, error } = useAPI(url + `?_limit=${limit}&_sort=id&_order=desc`);
+
+  const handleLoadMore = () => {
+    setProductLimit(productLimit + 24);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -18,18 +26,23 @@ function HomePage() {
 
   return (
     <div className="homePageContainer px-2">
-      <div className="homePageTitle--container my-5">
-        <Card className="homePageTitle--card d-flex p-4 flex-column justify-content-center align-items-center">
-          <h1>Here goes filters</h1>
+      <div className="homePageTitle--container mt-5 mb-4">
+        <Card className="homePageTitle--card d-flex p-2 flex-column justify-content-center align-items-center">
+          <h4>Here goes filters</h4>
         </Card>
       </div>
       <Row>
-        {data.map((product) => (
+        {data.slice(0, productLimit).map((product) => (
           <Col key={product.id} sm={12} md={6} lg={3} xl={4}>
             <ProductCard product={product} />
           </Col>
         ))}
       </Row>
+      {data.length > productLimit && (
+        <div className="text-center mb-5 pb-2">
+          <StyledButton onClick={handleLoadMore}>Load more</StyledButton>
+        </div>
+      )}
     </div>
   );
 }
